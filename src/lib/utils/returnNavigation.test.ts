c84f9fe94bb63e78b62161c628c2f688057e6adf
jayadityachawla04 <jayadityachawla04@gmail.com>
2023-03-04 19:12:23 +0800

    add src/lib/utils/returnNavigation.test.ts

---
+++ src/lib/utils/returnNavigation.test.ts
@@ -0,0 +1,97 @@
+import { describe, expect, it } from 'vitest'
+
+import {
+  resolveReturnTarget,
+  shouldUseHistoryBackForTarget,
+} from './returnNavigation'
+
+describe(resolveReturnTarget, () => {
+  it('returns fallback when returnTo is missing or invalid', () => {
+    const options = {
+      fallback: '/',
+      baseOrigin: 'http://localhost',
+      expectedPathname: '/',
+      invalidTabs: ['logs', 'map'],
+    } as const
+
+    expect(resolveReturnTarget(undefined, options)).toBe('/')
+    expect(resolveReturnTarget('   ', options)).toBe('/')
+    expect(resolveReturnTarget('https://evil.test', options)).toBe('/')
+    expect(resolveReturnTarget('//evil.test', options)).toBe('/')
+    expect(resolveReturnTarget('/traces/foo', options)).toBe('/')
+  })
+
+  it('applies tab normalization callback before returning', () => {
+    const result = resolveReturnTarget('/?tab=traces&search=checkout', {
+      fallback: '/',
+      baseOrigin: 'http://localhost',
+      expectedPathname: '/',
+      invalidTabs: ['logs', 'map'],
+      normalizeTab: (tab, searchParams) => {
+        if (tab === 'traces') {
+          searchParams.delete('tab')
+        }
+      },
+    })
+
+    expect(result).toBe('/?search=checkout')
+  })
+
+  it('returns fallback when tab is explicitly disallowed', () => {
+    const result = resolveReturnTarget('/?tab=map&search=checkout', {
+      fallback: '/',
+      baseOrigin: 'http://localhost',
+      expectedPathname: '/',
+      invalidTabs: ['logs', 'map'],
+    })
+
+    expect(result).toBe('/')
+  })
+
+  it('accepts a predicate function for expectedPathname', () => {
+    const options = {
+      fallback: '/?tab=logs',
+      baseOrigin: 'http://localhost',
+      expectedPathname: (p: string) => p === '/' || p.startsWith('/traces/'),
+      invalidTabs: [] as const,
+    }
+
+    expect(resolveReturnTarget('/?tab=logs', options)).toBe('/?tab=logs')
+    expect(resolveReturnTarget('/traces/abc123', options)).toBe(
+      '/traces/abc123',
+    )
+    expect(resolveReturnTarget('/traces/abc123?spanId=span-xyz', options)).toBe(
+      '/traces/abc123?spanId=span-xyz',
+    )
+    expect(resolveReturnTarget('/logs/log-001', options)).toBe('/?tab=logs')
+    expect(resolveReturnTarget('/admin', options)).toBe('/?tab=logs')
+  })
+})
+
+describe(shouldUseHistoryBackForTarget, () => {
+  it('returns true only when referrer exactly matches target URL parts', () => {
+    expect(
+      shouldUseHistoryBackForTarget(
+        'http://localhost/?search=checkout',
+        'http://localhost',
+        '/?search=checkout',
+      ),
+    ).toBe(true)
+
+    expect(
+      shouldUseHistoryBackForTarget(
+        'http://localhost/?search=checkout',
+        'http://localhost',
+        '/?search=payments',
+      ),
+    ).toBe(false)
+
+    expect(
+      shouldUseHistoryBackForTarget(
+        'https://example.com/?search=checkout',
+        'http://localhost',
+        '/?search=checkout',
+      ),
+    ).toBe(false)
+  })
+})
