697ec71e884d23870b0ee3d2b59cd90be8cfa7c9
jayadityachawla04 <jayadityachawla04@gmail.com>
2023-03-05 09:12:23 +0800

    add src/lib/utils/spans.ts

---
+++ src/lib/utils/spans.ts
@@ -0,0 +1,199 @@
+import type { StoredSpan, SpanTreeNode } from '$lib/types'
+
+// Map SpanKind integer to label
+export function spanKindLabel(kind: number): string {
+  switch (kind) {
+    case 0:
+      return 'UNSPECIFIED'
+    case 1:
+      return 'INTERNAL'
+    case 2:
+      return 'SERVER'
+    case 3:
+      return 'CLIENT'
+    case 4:
+      return 'PRODUCER'
+    case 5:
+      return 'CONSUMER'
+    default:
+      return 'UNKNOWN'
+  }
+}
+
+// Map Status code to label
+export function statusLabel(code: number): string {
+  switch (code) {
+    case 0:
+      return 'UNSET'
+    case 1:
+      return 'OK'
+    case 2:
+      return 'ERROR'
+    default:
+      return 'UNKNOWN'
+  }
+}
+
+// Build span tree from flat span array
+export function buildSpanTree(spans: StoredSpan[]): SpanTreeNode[] {
+  // Create a map of spanId -> span for quick lookup
+  const spanMap = new Map<string, StoredSpan>()
+  for (const span of spans) {
+    spanMap.set(span.spanId, span)
+  }
+
+  // Build parent -> children map
+  const childrenMap = new Map<string, StoredSpan[]>()
+  const rootSpans: StoredSpan[] = []
+  // Orphan spans grouped by their missing parentSpanId
+  const orphansByParent = new Map<string, StoredSpan[]>()
+
+  for (const span of spans) {
+    if (!span.parentSpanId || span.parentSpanId === '') {
+      // Root span (no parent)
+      rootSpans.push(span)
+    } else if (spanMap.has(span.parentSpanId)) {
+      // Child span with valid parent
+      const children = childrenMap.get(span.parentSpanId) || []
+      children.push(span)
+      childrenMap.set(span.parentSpanId, children)
+    } else {
+      // Orphan span: parent not found — group under a phantom placeholder
+      const orphans = orphansByParent.get(span.parentSpanId) || []
+      orphans.push(span)
+      orphansByParent.set(span.parentSpanId, orphans)
+    }
+  }
+
+  // Recursively build tree nodes
+  function buildNode(
+    span: StoredSpan,
+    depth: number,
+    visited: Set<string> = new Set(),
+  ): SpanTreeNode {
+    // Cycle detection: if we've already visited this span in this recursion path, skip it
+    if (visited.has(span.spanId)) {
+      return {
+        span,
+        depth,
+        children: [],
+        collapsed: false,
+        subtreeSize: 0,
+      }
+    }
+
+    // Mark this span as visited in the current path
+    const currentVisited = new Set(visited)
+    currentVisited.add(span.spanId)
+
+    const children = childrenMap.get(span.spanId) || []
+    // Sort children by start time
+    children.sort((a, b) =>
+      a.startTimeUnixNano.localeCompare(b.startTimeUnixNano),
+    )
+
+    const childNodes = children.map((child) =>
+      buildNode(child, depth + 1, currentVisited),
+    )
+    const subtreeSize = childNodes.reduce(
+      (sum, c) => sum + 1 + c.subtreeSize,
+      0,
+    )
+
+    return {
+      span,
+      depth,
+      children: childNodes,
+      collapsed: false,
+      subtreeSize,
+    }
+  }
+
+  // Sort root spans by start time
+  rootSpans.sort((a, b) =>
+    a.startTimeUnixNano.localeCompare(b.startTimeUnixNano),
+  )
+
+  const result: SpanTreeNode[] = rootSpans.map((span) => buildNode(span, 0))
+
+  // Build phantom placeholder nodes for each missing parent
+  for (const [missingParentId, orphans] of orphansByParent) {
+    orphans.sort((a, b) =>
+      a.startTimeUnixNano.localeCompare(b.startTimeUnixNano),
+    )
+    const startTimeUnixNano = orphans.reduce(
+      (min, s) => (s.startTimeUnixNano < min ? s.startTimeUnixNano : min),
+      orphans[0].startTimeUnixNano,
+    )
+    const endTimeUnixNano = orphans.reduce(
+      (max, s) => (s.endTimeUnixNano > max ? s.endTimeUnixNano : max),
+      orphans[0].endTimeUnixNano,
+    )
+    const phantomSpan: StoredSpan = {
+      traceId: orphans[0].traceId,
+      spanId: missingParentId,
+      parentSpanId: '',
+      name: '(missing)',
+      kind: 0,
+      startTimeUnixNano,
+      endTimeUnixNano,
+      attributes: {},
+      events: [],
+      links: [],
+      status: { code: 0, message: '' },
+      resource: {},
+      scopeName: '',
+      scopeVersion: '',
+      scopeAttributes: {},
+    }
+    const childNodes = orphans.map((orphan) => buildNode(orphan, 1))
+    const subtreeSize = childNodes.reduce(
+      (sum, c) => sum + 1 + c.subtreeSize,
+      0,
+    )
+    result.push({
+      span: phantomSpan,
+      depth: 0,
+      children: childNodes,
+      collapsed: false,
+      subtreeSize,
+      isPhantom: true,
+    })
+  }
+
+  // Sort all root-level nodes (real roots + phantoms) by start time
+  result.sort((a, b) =>
+    a.span.startTimeUnixNano.localeCompare(b.span.startTimeUnixNano),
+  )
+
+  return result
+}
+
+// Flatten tree to ordered list for rendering
+export function flattenSpanTree(nodes: SpanTreeNode[]): SpanTreeNode[] {
+  const result: SpanTreeNode[] = []
+
+  function traverse(node: SpanTreeNode) {
+    result.push(node)
+    if (!node.collapsed) {
+      for (const child of node.children) {
+        traverse(child)
+      }
+    }
+  }
+
+  for (const node of nodes) {
+    traverse(node)
+  }
+
+  return result
+}
+
+// Count total spans in subtree
+export function countSubtreeSpans(node: SpanTreeNode): number {
+  let count = 1 // count this node
+  for (const child of node.children) {
+    count += countSubtreeSpans(child)
+  }
+  return count
+}
