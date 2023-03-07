0b99c4f9b5ffce68642701a0b333337305a6607a
jayadityachawla04 <jayadityachawla04@gmail.com>
2023-03-07 15:13:23 +0800

    add src/routes/api/config/+server.ts

---
+++ src/routes/api/config/+server.ts
@@ -0,0 +1,12 @@
+// Returns server-side configuration values needed by the frontend
+import { json } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { getPersistenceStatus, traceStore } from '$lib/server/traceStore'
+
+export const GET: RequestHandler = async () => {
+  return json({
+    maxTraces: traceStore.maxTraces,
+    maxLogs: traceStore.maxLogs,
+    persistence: getPersistenceStatus(),
+  })
+}
