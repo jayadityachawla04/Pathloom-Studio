13fce94359c8a21c62d8823f4f861db548c66801
jayadityachawla04 <jayadityachawla04@gmail.com>
2023-03-11 03:18:23 +0800

    add src/routes/api/traces/[traceId]/logs/[logId]/+server.ts

---
+++ src/routes/api/traces/[traceId]/logs/[logId]/+server.ts
@@ -0,0 +1,19 @@
+import { json, error } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { traceStore } from '$lib/server/traceStore'
+
+export const GET: RequestHandler = async ({ params }) => {
+  const { traceId, logId } = params
+  const trace = traceStore.getTrace(traceId)
+
+  if (!trace) {
+    throw error(404, 'Trace not found')
+  }
+
+  const log = traceStore.getLog(logId)
+  if (!log || log.traceId !== traceId) {
+    throw error(404, 'Log not found')
+  }
+
+  return json(log)
+}
