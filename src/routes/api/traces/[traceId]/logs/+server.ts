a5e872c6fa6f4cb83520b1fb84b87da34d638286
jayadityachawla04 <jayadityachawla04@gmail.com>
2023-03-10 21:04:23 +0800

    add src/routes/api/traces/[traceId]/logs/+server.ts

---
+++ src/routes/api/traces/[traceId]/logs/+server.ts
@@ -0,0 +1,35 @@
+import { json, error } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { traceStore } from '$lib/server/traceStore'
+
+export const GET: RequestHandler = async ({ params, url }) => {
+  const { traceId } = params
+  const trace = traceStore.getTrace(traceId)
+
+  if (!trace) {
+    throw error(404, 'Trace not found')
+  }
+
+  const limitParam = url.searchParams.get('limit')
+  const spanIdFilter = url.searchParams.get('spanId')
+
+  const DEFAULT_LIMIT = 100
+  const MAX_LIMIT = 5000
+
+  let limit = DEFAULT_LIMIT
+  if (limitParam !== null) {
+    const parsed = Number.parseInt(limitParam, 10)
+    if (!Number.isNaN(parsed) && parsed > 0) {
+      limit = Math.min(parsed, MAX_LIMIT)
+    }
+  }
+
+  if (spanIdFilter) {
+    const logs = traceStore.getTraceLogs(traceId, MAX_LIMIT)
+    const filtered = logs.filter((log) => log.spanId === spanIdFilter)
+    return json(filtered.slice(0, limit))
+  }
+
+  const logs = traceStore.getTraceLogs(traceId, limit)
+  return json(logs)
+}
